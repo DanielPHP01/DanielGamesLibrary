8be8090cb8dca73fd6c251c950caaeae93e6af89
Dom <DANIELPHP01>
2023-05-08 22:54:13 +0600

    Update to version 0.0.5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -9,6 +9,7 @@ import static java.awt.Color.RED;
 import static java.awt.Color.YELLOW;
 
 public class Main {
+    tRI-dANIEL01
     public static void main(String[] args) {
         TicTacToe ticTacToe = new TicTacToe();
         ticTacToe.setBackgroundColor(RED);
